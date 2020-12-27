6045f8285bc106d825e233d37fc417bff7eae33f
mikohan <angara99@gmail.com>
2020-12-27 13:17:16 +0300

    Auth working

--- pages/authenticated.tsx
+++ pages/authenticated.tsx
@@ -6,35 +6,36 @@ import { firebaseClient } from "../firebaseClient";
 import { InferGetServerSidePropsType, GetServerSidePropsContext } from "next";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-  try {
-    const cookies = nookies.get(ctx);
-    console.log(JSON.stringify(cookies, null, 2));
-    const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
-    const { uid, email } = token;
+  //try {
+  const cookies = nookies.get(ctx);
+  console.log(JSON.stringify(cookies, null, 2));
+  const token = await firebaseAdmin.auth().verifyIdToken(cookies.token);
+  const { uid, email } = token;
 
-    // the user is authenticated!
-    // FETCH STUFF HERE
+  console.log(uid, email);
+  // the user is authenticated!
+  // FETCH STUFF HERE
 
-    return {
-      props: { message: `Your email is ${email} and your UID is ${uid}.` },
-    };
-  } catch (err) {
-    // either the `token` cookie didn't exist
-    // or token verification failed
-    // either way: redirect to the login page
-    // either the `token` cookie didn't exist
-    // or token verification failed
-    // either way: redirect to the login page
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/login",
-      },
-      // `as never` is required for correct type inference
-      // by InferGetServerSidePropsType below
-      props: {} as never,
-    };
-  }
+  return {
+    props: { message: `Your email is ${email} and your UID is ${uid}.` },
+    // };
+    /* } catch (err) { */
+    /*   // either the `token` cookie didn't exist */
+    /*   // or token verification failed */
+    /*   // either way: redirect to the login page */
+    /*   // either the `token` cookie didn't exist */
+    /*   // or token verification failed */
+    /*   // either way: redirect to the login page */
+    /*   return { */
+    /*     redirect: { */
+    /*       permanent: false, */
+    /*       destination: "/login", */
+    /*     }, */
+    /*     // `as never` is required for correct type inference */
+    /*     // by InferGetServerSidePropsType below */
+    /*     props: {} as never, */
+    /*   }; */
+  };
 };
 
 const AuthenticatedPage = (
